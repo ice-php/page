116a4b235efbcf038a5e4aa649686e23f53e7c3d
lanbing <liyong@nenu.edu.cn>
2018-09-11 10:47:53 +0800

    分页功能

--- src/functions.php
+++ src/functions.php
@@ -5,9 +5,12 @@ namespace icePHP;
 
 /**
  * 返回一个分页实例
+ * @param $size int 每页行数
+ * @param $order string 排序依据字段
+ * @param $dir string 排序方向
  * @return Page
  */
-function page()
+function page($size = null, $order = 'id', $dir = 'desc')
 {
-    return Page::instance();
+    return Page::instance($size, $order, $dir);
 }
